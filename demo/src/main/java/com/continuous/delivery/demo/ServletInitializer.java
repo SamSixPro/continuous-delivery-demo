ed1c6702e7f3e9700788107d2c58c9a34faae556
SamSixPro <samsix.pro@gmail.com>
2016-12-18 18:38:30 -0500

    Test.

--- demo/src/main/java/com/continuous/delivery/demo/ServletInitializer.java
+++ demo/src/main/java/com/continuous/delivery/demo/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * 
- * @author _
+ * @author _ Sami Kendri
  *
  */
 public class ServletInitializer extends SpringBootServletInitializer {
